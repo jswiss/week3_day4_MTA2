b8fa8a899d311df45ddc0758679e354510a0e333
Joshua Swiss <swissjoshua@gmail.com>
2015-08-27 20:02:52 +0100

    error checking working

--- file.rb
+++ file.rb
@@ -7,16 +7,36 @@
 puts "What train are you riding #{trains.keys}?"
 train1 = gets.chomp.capitalize
 
+while ! trains.include? train1
+  puts "What train are you riding #{trains.keys}?"
+  train1 = gets.chomp.capitalize
+end  
+
 puts "Where are you starting #{trains[train1]}?"
 start = gets.chomp.downcase
+
+while ! trains[train1].include? start
+  puts "Where are you starting #{trains[train1]}?"
+  start = gets.chomp.downcase  
+end
 
 start_index = trains[train1].index(start)
 
 puts "What train are you arriving with #{trains.keys}?"
-train2 = gets.chomp.capitalize.downcase
+train2 = gets.chomp.capitalize.capitalize
+
+while ! trains.include? train2
+  puts "What train are you arriving with #{trains.keys}?"
+  train2 = gets.chomp.capitalize.capitalize
+end
 
 puts "Where are you finishing your journey #{trains[train2]}?"
-stop = gets.chomp
+stop = gets.chomp.downcase
+
+while ! trains[train2].include? stop
+  puts "Where are you finishing your journey #{trains[train2]}?"
+  stop = gets.chomp.downcase
+end
 
 stop_index = trains[train2].index(stop) 
 
