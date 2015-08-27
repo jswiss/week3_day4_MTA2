8fc347a3a47a066a376329c245b466fc3486089f
Joshua Swiss <swissjoshua@gmail.com>
2015-08-27 16:28:06 +0100

    added hashes

---
+++ file.rb
@@ -0,0 +1,18 @@
+trains = {
+  "N" => {
+    :stops => ['ts', '34', '28-n', '23-n', 'us', '8-n']
+  },
+  "L" => {
+    :stops => ['8-L', '6', 'us', '3', '1']
+  },
+  "6" => {
+    :stops => ['gc', '33', '28-6', '23-6', 'us', 'ap']
+  }
+}
+
+train = ''
+
+puts 'What train are you riding?'
+train = gets
+
+
