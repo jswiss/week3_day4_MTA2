24ce7e54e978f0216e9447a204fe66de214dac6d
Joshua Swiss <swissjoshua@gmail.com>
2015-08-27 20:22:04 +0100

    until loop working

--- file.rb
+++ file.rb
@@ -4,55 +4,65 @@
       '6' => ['gc', '33', '28-6', '23-6', 'us', 'ap']
     }
 
-puts "What train are you riding #{trains.keys}?"
-train1 = gets.chomp.capitalize
+puts "Going somewhere (yes or no)?"
+trip = gets.chomp
 
-while ! trains.include? train1
+until trip == 'no'
   puts "What train are you riding #{trains.keys}?"
   train1 = gets.chomp.capitalize
-end  
 
-puts "Where are you starting #{trains[train1]}?"
-start = gets.chomp.downcase
+  while ! trains.include? train1
+    puts "What train are you riding #{trains.keys}?"
+    train1 = gets.chomp.capitalize
+  end  
 
-while ! trains[train1].include? start
   puts "Where are you starting #{trains[train1]}?"
-  start = gets.chomp.downcase  
-end
+  start = gets.chomp.downcase
 
-start_index = trains[train1].index(start)
+  while ! trains[train1].include? start
+    puts "Where are you starting #{trains[train1]}?"
+    start = gets.chomp.downcase  
+  end
 
-puts "What train are you arriving with #{trains.keys}?"
-train2 = gets.chomp.capitalize.capitalize
+  start_index = trains[train1].index(start)
 
-while ! trains.include? train2
   puts "What train are you arriving with #{trains.keys}?"
   train2 = gets.chomp.capitalize.capitalize
-end
 
-puts "Where are you finishing your journey #{trains[train2]}?"
-stop = gets.chomp.downcase
+  while ! trains.include? train2
+    puts "What train are you arriving with #{trains.keys}?"
+    train2 = gets.chomp.capitalize.capitalize
+  end
 
-while ! trains[train2].include? stop
   puts "Where are you finishing your journey #{trains[train2]}?"
   stop = gets.chomp.downcase
+
+  while ! trains[train2].include? stop
+    puts "Where are you finishing your journey #{trains[train2]}?"
+    stop = gets.chomp.downcase
+  end
+
+  stop_index = trains[train2].index(stop) 
+
+  same_train_answer = start_index - stop_index 
+
+  multi_train_start_index = trains[train1].index('us') - start_index
+  multi_train_stop_index =  trains[train2].index('us') - stop_index
+  multi_train_answer = multi_train_start_index.abs + multi_train_stop_index.abs
+
+  if train1 == train2
+    puts "You have #{same_train_answer.abs} stops to reach your destination"
+  else
+    puts "You have #{multi_train_answer} stops to reach your destination"
+  end
+puts 'Going somewhere else (yes or no)?'
+trip2 = gets.chomp
+
+if trip2 == 'no'
+  break
 end
 
-stop_index = trains[train2].index(stop) 
-
-same_train_answer = start_index - stop_index 
-
-multi_train_start_index = trains[train1].index('us') - start_index
-multi_train_stop_index =  trains[train2].index('us') - stop_index
-multi_train_answer = multi_train_start_index.abs + multi_train_stop_index.abs
-
-if train1 == train2
-  puts "You have #{same_train_answer.abs} stops to reach your destination"
-else
-  puts "You have #{multi_train_answer} stops to reach your destination"
-
 end
-
 
 
 
