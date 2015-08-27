dbe4acf2110b42b31f10256d29863fa0188395d4
Joshua Swiss <swissjoshua@gmail.com>
2015-08-27 21:07:10 +0100

    changed train variable to mta

--- file.rb
+++ file.rb
@@ -1,4 +1,4 @@
-  trains = { 
+  mta = { 
       'N' => ['ts', '34', '28-n', '23-n', 'us', '8-n'],
       'L' => ['8-L', '6', 'us', '3', '1'],
       '6' => ['gc', '33', '28-6', '23-6', 'us', 'ap']
@@ -8,46 +8,46 @@ puts "Going somewhere (yes or no)?"
 trip = gets.chomp
 
 until trip == 'no'
-  puts "What train are you riding #{trains.keys}?"
+  puts "What train are you riding #{mta.keys}?"
   train1 = gets.chomp.capitalize
 
-  while ! trains.include? train1
-    puts "What train are you riding #{trains.keys}?"
+  while ! mta.include? train1
+    puts "What train are you riding #{mta.keys}?"
     train1 = gets.chomp.capitalize
   end  
 
-  puts "Where are you starting #{trains[train1]}?"
+  puts "Where are you starting #{mta[train1]}?"
   start = gets.chomp.downcase
 
-  while ! trains[train1].include? start
-    puts "Where are you starting #{trains[train1]}?"
+  while ! mta[train1].include? start
+    puts "Where are you starting #{mta[train1]}?"
     start = gets.chomp.downcase  
   end
 
-  start_index = trains[train1].index(start)
+  start_index = mta[train1].index(start)
 
-  puts "What train are you arriving with #{trains.keys}?"
+  puts "What train are you arriving with #{mta.keys}?"
   train2 = gets.chomp.capitalize.capitalize
 
-  while ! trains.include? train2
-    puts "What train are you arriving with #{trains.keys}?"
+  while ! mta.include? train2
+    puts "What train are you arriving with #{mta.keys}?"
     train2 = gets.chomp.capitalize.capitalize
   end
 
-  puts "Where are you finishing your journey #{trains[train2]}?"
+  puts "Where are you finishing your journey #{mta[train2]}?"
   stop = gets.chomp.downcase
 
-  while ! trains[train2].include? stop
-    puts "Where are you finishing your journey #{trains[train2]}?"
+  while ! mta[train2].include? stop
+    puts "Where are you finishing your journey #{mta[train2]}?"
     stop = gets.chomp.downcase
   end
 
-  stop_index = trains[train2].index(stop) 
+  stop_index = mta[train2].index(stop) 
 
   same_train_answer = start_index - stop_index 
 
-  multi_train_start_index = trains[train1].index('us') - start_index
-  multi_train_stop_index =  trains[train2].index('us') - stop_index
+  multi_train_start_index = mta[train1].index('us') - start_index
+  multi_train_stop_index =  mta[train2].index('us') - stop_index
   multi_train_answer = multi_train_start_index.abs + multi_train_stop_index.abs
 
   if train1 == train2
